345c5d15c026e151f652fc2950e2c9658ff5c492
XuWang <wangxu724@gmail.com>
2018-08-02 19:26:17 -0700

    Add android/ios project

---
+++ android/app/src/main/java/com/weekly_countdown_rn/MainActivity.java
@@ -0,0 +1,15 @@
+package com.weekly_countdown_rn;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "weekly_countdown_rn";
+    }
+}
