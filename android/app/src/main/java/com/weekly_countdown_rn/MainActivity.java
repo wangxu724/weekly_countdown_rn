39dd407a69bf991f6333deeb9fc772599a4ab320
XuWang <wangxu724@gmail.com>
2018-08-11 18:32:51 -0700

    tiny change

--- android/app/src/main/java/com/weekly_countdown_rn/MainActivity.java
+++ android/app/src/main/java/com/weekly_countdown_rn/MainActivity.java
@@ -38,6 +38,5 @@ public class MainActivity extends ReactActivity {
             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             | View.SYSTEM_UI_FLAG_FULLSCREEN);
-
     }
 }
